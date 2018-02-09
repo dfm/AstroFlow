4d90e4b5dcc3d0ed6ccd37e12f9d75f506a16cab
Dan F-M <danfm@nyu.edu>
2018-02-09 13:38:24 -0500

    cleanup

---
+++ astroflow/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+__all__ = [
+    "kepler",
+    "searchsorted", "interp"
+]
+
+from .astroflow import (
+    kepler,
+    searchsorted, interp,
+)
