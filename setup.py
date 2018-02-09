7b11657d4781c73bf581c03666644a98cbaf9da1
Dan F-M <danfm@nyu.edu>
2018-02-09 14:44:31 -0500

    starting to build support for GPU

--- setup.py
+++ setup.py
@@ -2,14 +2,52 @@
 
 import os
 import sys
+import logging
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
 
 
+def find_in_path(name, path):
+    for d in path.split(os.pathsep):
+        binpath = os.path.join(d, name)
+        if os.path.exists(binpath):
+            return os.path.abspath(binpath)
+    return None
+
+
+def customize_compiler_for_nvcc(compiler, nvcc):
+    """Borrowed from: https://github.com/rmcgibbo/npcuda-example"""
+    compiler.src_extensions.append('.cu')
+    default_compiler_so = compiler.compiler_so
+    default_compile = compiler._compile
+
+    def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
+        if os.path.splitext(src)[1] == '.cu':
+            compiler.set_executable('compiler_so', nvcc)
+            postargs = extra_postargs['nvcc']
+        else:
+            postargs = extra_postargs['gcc']
+
+        default_compile(obj, src, ext, cc_args, postargs, pp_opts)
+        compiler.compiler_so = default_compiler_so
+
+    compiler._compile = _compile
+
+
 class build_ext(_build_ext):
 
     def build_extension(self, ext):
+        import tensorflow as tf
+        from tensorflow.python.framework.test_util import IsGoogleCudaEnabled
+
+        if IsGoogleCudaEnabled():
+            nvcc = find_in_path("nvcc", os.environ["PATH"])
+            if nvcc is None:
+                logging.warn("nvcc couldn't be found on your PATH")
+            else:
+                customize_compiler_for_nvcc(self.compiler, nvcc)
+
         include_dirs = ["astroflow", os.path.join("astroflow", "ops")]
         library_dirs = []
         libraries = []
@@ -23,7 +61,6 @@ class build_ext(_build_ext):
             libraries += ["m", "c++", "stdc++"]
 
         # Link to TensorFlow
-        import tensorflow as tf
         try:
             # This is new in v1.5
             compile_flags += tf.sysconfig.get_compile_flags()
